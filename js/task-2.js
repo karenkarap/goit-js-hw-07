bbc267ebbfa6235474d91e9e06bca741c77b70f5
karenkarap <myspecspace@gmail.com>
2025-07-05 18:45:03 +0400

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,37 @@
+class Storage {
+  #items = [];
+  constructor(array) {
+    this.#items.push(...array);
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    // ~ first option
+    // this.#items = this.#items.filter(item => item !== itemToRemove);
+
+    // ~ second option
+    let i = this.#items.indexOf(itemToRemove);
+    if (i !== -1) {
+      this.#items.splice(i, 1);
+    }
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
